fdb67a0e6707e1049f4ce6f93c9f7a597d97ebcd
Programador Showzim <programadorshowzim@gmail.com>
2022-10-21 22:10:08 -0300

    Adicionando telefone e nome no devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :phone)
+    end
+  end
 end
