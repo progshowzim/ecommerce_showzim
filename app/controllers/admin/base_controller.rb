fa1602c4107517bb39cd002f279a9a0d2ae54423
Programador Showzim <programadorshowzim@gmail.com>
2022-10-28 22:10:06 -0300

    Restringindo acesso do admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,13 @@
 module Admin
   class BaseController < ApplicationController
     layout 'admin'
+    before_action :authenticate_user!
+    before_action :only_admin!
+
+    private
+
+    def only_admin!
+      redirect_to root_path unless current_user.admin?
+    end
   end
 end
