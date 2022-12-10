0268235af69e11e28ff33184668c491ef8c1ff68
Italo Matos <italomatos@gmail.com>
2022-12-10 12:53:32 -0300

    refactory product decorator

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -1,9 +1,9 @@
 class ProductDecorator < SimpleDelegator
-  def full_promo
-    if promo?
-      "<span class=\"text-muted text-decoration-line-through\">#{ActionController::Base.helpers.number_to_currency price}</span> #{ActionController::Base.helpers.number_to_currency promo_price}".html_safe
-    else
-      ActionController::Base.helpers.number_to_currency price
-    end
+  include ActionView::Helpers::NumberHelper
+  
+  def price
+    return number_to_currency(super) unless promo?
+
+    "<span class=\"text-muted text-decoration-line-through\">#{number_to_currency(super)}</span> #{number_to_currency(promo_price)}".html_safe
   end
 end
