53732af8db4023a6a819e4159cbe44541f453759
Programador Showzim <programadorshowzim@gmail.com>
2023-02-17 22:01:06 -0300

    Start CRUD of Addresses

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,3 +1,35 @@
 class AddressesController < ApplicationController
-  def index; end
+  def index
+    @addresses = current_user.addresses
+  end
+
+  def new
+    @address = current_user.addresses.new
+  end
+
+  def create
+    @address = current_user.addresses.new(address_params)
+    if @address.save
+      flash[:notice] = 'Endereço cadastrado com sucesso!'
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def address_params
+    params.require(:address).permit(
+      :name, :street, :neighborhood, :number, :address_detail, :zipcode, :city, :state)
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
 end
